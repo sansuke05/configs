43523fd9efadb7f278d1eda6ee2399e0f570b2c1
Takanori Watanabe <takanori5064@gmail.com>
2017-11-09 17:16:58 +0900

    .configディレクトリを追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+# prompt
+
+# java alias
+alias javac='javac -J-Dfile.encoding=UTF-8'
+alias java='java -Dfile.encoding=UTF-8'
+alias java_home='/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home -V'
+
+# ssh alias
+alias ssh_aizu='ssh -Y s1230042@sshgate.u-aizu.ac.jp'
+
+# android sdk location
+set PATH /Users/user/Library/Android/sdk/platform-tools $PATH
