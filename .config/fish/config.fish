a2753b711bdec8c97901d6fcff5c5a6b4875006b
Takanori Watanabe <takanori5064@gmail.com>
2018-01-07 15:53:02 +0900

    config.fishを編集

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,9 @@ alias java_home='/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands
 alias ssh_aizu='ssh -Y s1230042@sshgate.u-aizu.ac.jp'
 
 # android sdk location
-set PATH /Users/user/Library/Android/sdk/platform-tools $PATH
+set PATH $HOME/Library/Android/sdk/platform-tools $PATH
+set ANDROID_HOME $HOME/Library/Android/sdk
+
+# command alias
+alias tree='pwd;find . | sort | sed '\''1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'\'''
+
